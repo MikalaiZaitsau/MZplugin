7d78528d43170538a9b8a382ea2fd7d5cdf0b8ac
Mikalai Zaitsau <dictumfactum.zn@gmail.com>
2023-09-26 16:44:34 +0300

    First Command

---
+++ MZplugin/MainCommand.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace MZplugin
+{
+    [Transaction(TransactionMode.Manual)]
+    public class MainCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Приветствие", "Hi, my friend!");
+            
+            return Result.Succeeded;
+        }
+    }
+}
